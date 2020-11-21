51d209ef3ecd2ebe3df0bc55b0d72eeec3a82706
bianca <b.cosma@student.tudelft.nl>
2020-11-21 19:34:41 +0100

    Change order of imports for PostTest class

--- src/test/java/nl/tudelft/sem/template/model/PostTest.java
+++ src/test/java/nl/tudelft/sem/template/model/PostTest.java
@@ -1,11 +1,14 @@
 package nl.tudelft.sem.template.model;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.LocalDateTime;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class PostTest {
     transient Post demoPost1;
