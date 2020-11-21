9a5877b6c74d1c7fa82c7bbaedc4bc0678ba827c
bianca <b.cosma@student.tudelft.nl>
2020-11-21 18:47:14 +0100

    Add constructors, getters and setters for Post class

--- src/main/java/nl/tudelft/sem/template/model/Post.java
+++ src/main/java/nl/tudelft/sem/template/model/Post.java
@@ -1,4 +1,74 @@
 package nl.tudelft.sem.template.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private int postNumber;
+
+    private String body;
+
+    private LocalDateTime created;
+
+    private LocalDateTime edited;
+
+    public Post() {
+    }
+
+    public Post(long id, int postNumber, String body, LocalDateTime created) {
+        this.id = id;
+        this.postNumber = postNumber;
+        this.body = body;
+        this.created = created;
+        //initially, set this field the same as the created field
+        this.edited = created;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getPostNumber() {
+        return postNumber;
+    }
+
+    public void setPostNumber(int postNumber) {
+        this.postNumber = postNumber;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
+    public LocalDateTime getEdited() {
+        return edited;
+    }
+
+    public void setEdited(LocalDateTime edited) {
+        this.edited = edited;
+    }
 }
