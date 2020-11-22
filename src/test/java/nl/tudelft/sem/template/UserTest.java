24cf67e02d7ed57f3b77f810ac0be0b5f2388033
“Bente <b.c.p.zuurbier@student.tudelft.nl>
2020-11-22 19:16:39 +0100

    Add tests for equals, toString, hashcode and made fields transient.

--- src/test/java/nl/tudelft/sem/template/UserTest.java
+++ src/test/java/nl/tudelft/sem/template/UserTest.java
@@ -1,16 +1,17 @@
 package nl.tudelft.sem.template;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 public class UserTest {
-    User user1;
-    User user2;
-    Strign username1;
-    String password1;
-    String email1;
-    boolean type1;
+    transient User user1;
+    transient User user2;
+    transient Strign username1;
+    transient String password1;
+    transient String email1;
+    transient boolean type1;
 
     /**
      * Initialises the testing objects.
@@ -80,6 +81,70 @@ public class UserTest {
     void testGetAndSetUsername() {
         user2.setType(true);
         assertEquals(true, user2.isType());
+    }
+
+    /**
+     * Tests if the same objects will be equal.
+     */
+    @Test
+    void testEqualsSameObject() {
+        assertTrue(user2.equals(user2));
+    }
+
+    /**
+     * Tests if a copy of the object will be equal.
+     */
+    @Test
+    void testEqualsTrue() {
+        user2Copy = new Post(user2.getUserName(), user2.getPassword(), user2.getEmail(),
+                user2.getType());
+        assertTrue(user2Copy.equals(user2));
+    }
+
+    /**
+     * Tests if different initialized objects are not equal.
+     */
+    @Test
+    void testEqualsFalse() {
+        assertFalse(user2.equals(user1));
+    }
+
+    /**
+     * Tests if differten types of objects are not equal.
+     */
+    @Test
+    void testEqualsDifferentClass() {
+        assertFalse(user2.equals("A string."));
+    }
+
+    /**
+     * Tests if two different objects hae different hashcodes.
+     */
+    @Test
+    void testDifferentHashCodes() {
+        assertNotEquals(user1.hashCode(), user2.hashCode());
+    }
+
+    /**
+     * Tests if the same object has the same hashcode.
+     */
+    @Test
+    void testSameHashCodes() {
+        assertEquals(user1.hashCode(), user2.hashCode());
+    }
+
+    /**
+     * Tests if the toString method works.
+     */
+    @Test
+    void testToString() {
+        String string = "User{" +
+                "username='" + "Test" + '\'' +
+                ", password='" + "pwd" + '\'' +
+                ", email='" + "test@gmail.com" + '\'' +
+                ", type=" + "false" +
+                '}';
+        assertEquals(string, user2.toString());
     }
 
 
