ecf228fca94acba11a0a3276d5cc161f4bf4efc9
Pietro Vigilanza Lorenzo <p.m.vigilanzalorenzo@student.tudelft.nl>
2020-11-24 15:10:40 +0100

    Eureka server base implementation
    This commit includes a lot of dependencies requried
    to run the eurkea server so that we can start connecting
    our microservices.
    
    To Do: Check that actual setup is correct.

--- src/main/java/nl/tudelft/sem/template/Application.java
+++ src/main/java/nl/tudelft/sem/template/Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
+        System.setProperty("spring.config.name", "application");
         SpringApplication.run(Application.class, args);
     }
 }
