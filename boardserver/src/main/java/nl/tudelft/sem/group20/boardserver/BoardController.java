9644b566426a3561f4d78d14867475430abde7c0
bianca <b.cosma@student.tudelft.nl>
2020-11-30 19:42:38 +0100

    Add ResponseEntities for BoardController methods.

--- boardserver/src/main/java/nl/tudelft/sem/group20/boardserver/BoardController.java
+++ boardserver/src/main/java/nl/tudelft/sem/group20/boardserver/BoardController.java
@@ -4,6 +4,8 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,9 +29,14 @@ public class BoardController {
      */
     @PostMapping("/create")
     @ResponseBody
-    public Map<String, Long> createBoard(@RequestBody Board board) {
+    public ResponseEntity<?> createBoard(@RequestBody Board board) {
+        long assignedId = boardService.createBoard(board);
 
-        return Collections.singletonMap("ID", boardService.createBoard(board));
+        //board with this id already exists, no board was created
+        if (assignedId == -1)
+            return new ResponseEntity<>("A board with this id already exists.", HttpStatus.CONFLICT);
+
+        return new ResponseEntity<>(assignedId, HttpStatus.CREATED);
     }
 
     /**
@@ -39,9 +46,8 @@ public class BoardController {
      */
     @GetMapping("/get")
     @ResponseBody
-    public List<Board> getBoards() {
-
-        return boardService.getBoards();
+    public ResponseEntity<?> getBoards() {
+        return new ResponseEntity<>(boardService.getBoards(), HttpStatus.OK);
     }
 
     /**
@@ -52,9 +58,13 @@ public class BoardController {
      */
     @PostMapping("/edit")
     @ResponseBody
-    public Map<String, Boolean> editBoard(@RequestBody Board board) {
+    public ResponseEntity<?> editBoard(@RequestBody Board board) {
+        boolean updatedSucceeded = boardService.updateBoard(board);
 
-        return Collections.singletonMap("success", boardService.updateBoard(board));
+        if (updatedSucceeded)
+            return new ResponseEntity<>("This board does not exist.", HttpStatus.BAD_REQUEST);
+        else
+            return new ResponseEntity<>("The board was successfully updated.", HttpStatus.OK);
     }
 
 
