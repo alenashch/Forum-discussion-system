a25e73039067e383895b51415e385180b0ee3cf2
mokon <m.t.okon@student.tudelft.nl>
2020-12-16 15:06:21 +0100

    Add missing lines to the ContentControllerAdviceTest

--- contentserver/src/test/java/nl/tudelft/sem/group20/contentserver/controller/ContentControllerAdviceTest.java
+++ contentserver/src/test/java/nl/tudelft/sem/group20/contentserver/controller/ContentControllerAdviceTest.java
@@ -1,5 +1,6 @@
 package nl.tudelft.sem.group20.contentserver.controller;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
@@ -8,31 +9,41 @@ import org.springframework.http.ResponseEntity;
 class ContentControllerAdviceTest {
 
     private transient String message;
+    private transient RuntimeException exception;
+    private transient ContentControllerAdvice controllerAdvice;
 
     @BeforeEach
     void setUp() {
 
         message = "error";
+        exception = new RuntimeException(message);
+        controllerAdvice = new ContentControllerAdvice();
     }
 
     @Test
-    void handleAuthorizationFailedException() {
+    void handleAuthorizationFailedExceptionTest() {
 
         ResponseEntity<String> responseEntity = new ResponseEntity<>(message,
             HttpStatus.UNAUTHORIZED);
+        Assertions.assertEquals(responseEntity,
+            controllerAdvice.handleAuthorizationFailedException(exception));
     }
 
     @Test
-    void handleBoardThreadNotFoundException() {
+    void handleBoardThreadNotFoundExceptionTest() {
 
         ResponseEntity<String> responseEntity = new ResponseEntity<>(message,
             HttpStatus.BAD_REQUEST);
+        Assertions.assertEquals(responseEntity,
+            controllerAdvice.handleBoardThreadNotFoundException(exception));
     }
 
     @Test
-    void handlePostNotFoundException() {
+    void handlePostNotFoundExceptionTest() {
 
         ResponseEntity<String> responseEntity = new ResponseEntity<>(message,
             HttpStatus.BAD_REQUEST);
+        Assertions.assertEquals(responseEntity,
+            controllerAdvice.handlePostNotFoundException(exception));
     }
 }
