b6713a49bd49be5cc2dda57d0433f01a50203139
ashcheglova <a.shcheglova@student.tudelft.nl>
2021-01-18 19:05:35 +0300

    add ExtendedBaseEntityTest to improve mutation test coverage

---
+++ boardserver/src/test/java/nl/tudelft/sem/group20/boardserver/test/ExtendedBaseEntityTest.java
@@ -0,0 +1,77 @@
+package nl.tudelft.sem.group20.boardserver.test;
+
+import nl.tudelft.sem.group20.boardserver.embeddable.TimestampTracker;
+import nl.tudelft.sem.group20.boardserver.entities.Board;
+import nl.tudelft.sem.group20.boardserver.entities.ExtendedBaseEntity;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ExtendedBaseEntityTest {
+    transient ExtendedBaseEntity extendedBaseEntity;
+    transient ExtendedBaseEntity extendedBaseEntity2;
+    transient ExtendedBaseEntity extendedBaseEntityCopy;
+
+    @BeforeEach
+    void initialize() {
+        extendedBaseEntity = new ExtendedBaseEntity(1,"Chandler", "Chan");
+        extendedBaseEntity2 = new ExtendedBaseEntity(2,"Monica", "Mon");
+
+        extendedBaseEntityCopy = new ExtendedBaseEntity(extendedBaseEntity.getId(),
+                extendedBaseEntity.getName(), extendedBaseEntity.getUsername());
+        extendedBaseEntityCopy.setTimestampTracker(extendedBaseEntity.getTimestampTracker());
+    }
+
+    @Test
+    public void testSetCreated() {
+        extendedBaseEntity.getTimestampTracker().setCreated(null);
+        extendedBaseEntity.checkCreationTime(extendedBaseEntity);
+        assertEquals(extendedBaseEntity.getTimestampTracker().getCreated(), LocalDateTime.now());
+    }
+
+
+    @Test
+    public void testEntityTwoEqual() {
+        assertTrue(extendedBaseEntity.equals(extendedBaseEntityCopy));
+    }
+
+    @Test
+    public void testEntityTwoNotEqual() {
+        assertFalse(extendedBaseEntity.equals(extendedBaseEntity2));
+    }
+
+
+    @Test
+    void testEntityEqualsDifferentClass() {
+        assertFalse(extendedBaseEntity.equals("A string."));
+    }
+
+
+    @Test
+    void testDifferentHashCodes() {
+        assertNotEquals(extendedBaseEntity.hashCode(), extendedBaseEntity2.hashCode());
+    }
+
+    @Test
+    void testSameHashCodes() {
+        Assertions.assertEquals(extendedBaseEntity.hashCode(), extendedBaseEntityCopy.hashCode());
+    }
+
+
+    @Test
+    public void testEntityToString() {
+        String entityToString = "BaseEntity{"
+                + "id=" + extendedBaseEntity.getId()
+                + ", name='" + extendedBaseEntity.getName() + '\''
+                + ", username='" + extendedBaseEntity.getUsername() + '\''
+                + ", " + extendedBaseEntity.getTimestampTracker().toString()
+                + '}';
+        Assertions.assertEquals(extendedBaseEntity.toString(), entityToString);
+    }
+
+}
