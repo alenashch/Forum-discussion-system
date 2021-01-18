81c72310c1c33a2d4abcf6abb4cb54bb84d5b26a
bianca <b.cosma@student.tudelft.nl>
2021-01-18 15:25:24 +0100

    Add tests for hashcode and toString

--- boardserver/src/test/java/nl/tudelft/sem/group20/boardserver/test/TimestampTrackerTest.java
+++ boardserver/src/test/java/nl/tudelft/sem/group20/boardserver/test/TimestampTrackerTest.java
@@ -1,4 +1,42 @@
 package nl.tudelft.sem.group20.boardserver.test;
 
+import nl.tudelft.sem.group20.boardserver.embeddable.TimestampTracker;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
 public class TimestampTrackerTest {
+    transient TimestampTracker demoTracker;
+    transient TimestampTracker demoTrackerDifferentHashcode;
+    transient LocalDateTime newCreationTime;
+
+    @Test
+    public void testSetCreated() {
+        demoTracker = new TimestampTracker();
+        newCreationTime = demoTracker.getCreated().minusHours(3);
+        demoTracker.setCreated(newCreationTime);
+
+        assertEquals(newCreationTime, demoTracker.getCreated());
+    }
+
+    @Test
+    public void testHashcode() {
+        demoTracker = new TimestampTracker();
+        demoTrackerDifferentHashcode = new TimestampTracker();
+        demoTrackerDifferentHashcode.setCreated(LocalDateTime.now().minusDays(1));
+
+        assertNotEquals(demoTracker.hashCode(), demoTrackerDifferentHashcode.hashCode());
+    }
+
+    @Test
+    public void testToString() {
+        demoTracker = new TimestampTracker();
+        String result = "created=" + demoTracker.getCreated()
+                + ", edited=" + demoTracker.getEdited();
+
+        assertEquals(result, demoTracker.toString());
+    }
 }
